011edc804e9615d0f88350ef0251a046cf7f6c8a
Felipe Buch <ubuntu@ip-172-31-29-95.us-west-2.compute.internal>
2013-07-11 02:12:21 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,10 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFile('index.html', function (err, data) {
-      if (err) throw err;
-      console.log(data);
-      });
+  response.send(fs.readFileSync('index.html'));
 });
 
 var port = process.env.PORT || 5000;
